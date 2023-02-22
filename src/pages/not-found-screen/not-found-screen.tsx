448e12286616176df7c4d1504d026b10b2f19769
slepec <slepec2018@gmail.com>
2023-02-22 12:39:57 +0000

    home_task_3

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom';
+import {Helmet} from 'react-helmet-async';
+import Logo from '../../components/logo/logo';
+
 function NotFoundScreen(): JSX.Element {
   return (
     <section className="game">
-      <header className="game__header">
-        <a className="game__back" href="/">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
 
+      <header className="game__header">
+        <Logo />
+        <Helmet>
+          <title>Угадай мелодию. Страница не найдена</title>
+        </Helmet>
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx="390" cy="390" r="370"
             style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}
@@ -16,7 +20,7 @@ function NotFoundScreen(): JSX.Element {
 
       <section className="game__screen">
         <h1>404. Page not found</h1>
-        <a href="/">Вернуться на главную</a>
+        <Link to="/">Вернуться на главную</Link>
       </section>
     </section>
   );
